a1f415c00e4e9e527ed0979b5827c0f3a34b8987
MANISH-GIRI <manish.giri.me@gmail.com>
2017-06-19 21:19:49 -0400

    Submit Is this a triangle

---
+++ JavaScript/Is-This-A-Triangle.js
@@ -0,0 +1,14 @@
+/**
+ * Created by manishgiri on 6/19/17.
+ */
+
+/*
+ Implement a method that accepts 3 integer values a, b, c. The method should return true if a triangle can be built with the sides of given length and false in any other case.
+
+ (In this case, all triangles must have surface greater than 0 to be accepted).
+ */
+
+function isTriangle(a,b,c)
+{
+    return (a + b > c) && (a + c > b) && (b + c > a);
+}
