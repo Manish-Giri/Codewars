ac9fbd70e296ba2b18bf641c743b752328723249
Manish Giri <manish.giri.me@gmail.com>
2017-08-07 22:22:01 -0400

    Solve calculate average (JS)

---
+++ JavaScript/calculate-average.js
@@ -0,0 +1,6 @@
+// Write function avg which calculates average of numbers in given list.
+
+function find_average(array) {
+  // your code here
+  return array.reduce((a,b) => a + b)/array.length;
+}
