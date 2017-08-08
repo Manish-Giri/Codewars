fb1a59647b79d7203998f8f153d5a8f095769c3b
Manish Giri <manish.giri.me@gmail.com>
2017-08-07 21:14:09 -0400

    Solve find first non consecutive number in Java

---
+++ Java/find-the-first-non-consecutive-number.java
@@ -0,0 +1,10 @@
+class FirstNonConsecutive {
+    static Integer find(final int[] array) {
+      for(int i = 1; i <array.length; i++) {
+        if(Math.abs(array[i] - array[i-1]) > 1) {
+            return array[i];
+        }
+      }
+        return null;
+    }
+}
