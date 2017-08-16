f22e168107c941a0b7b7c400634563118dd43d6b
Manish Giri <manish.giri.me@gmail.com>
2017-08-16 03:40:00 -0400

    Solve invert values with Ruby

---
+++ Ruby/invert-values.rb
@@ -0,0 +1,11 @@
+=begin
+Given a set of numbers, return the additive inverse of each. Each positive becomes negatives, and the negatives become positives.
+
+invert([1,2,3,4,5]) == [-1,-2,-3,-4,-5]
+invert([1,-2,3,-4,5]) == [-1,2,-3,4,-5]
+invert([]) == []
+=end
+
+def invert(list)
+  list.map { |n| 0 - n }
+end
