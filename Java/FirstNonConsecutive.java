4954b36f73ae3ddbb75beaca6c9d4babad01ae95
MANISH-GIRI <manish.giri.me@gmail.com>
2017-08-16 04:11:15 -0400

    Commit Java first non consecutive

--- Java/FirstNonConsecutive.java
+++ Java/FirstNonConsecutive.java
@@ -1,5 +1,5 @@
 class FirstNonConsecutive {
-    static Integer find(final int[] array) {
+    private static Integer find(final int[] array) {
       for(int i = 1; i <array.length; i++) {
         if(Math.abs(array[i] - array[i-1]) > 1) {
             return array[i];
