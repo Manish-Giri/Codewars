b45a2683b1382bb99a091fc03e2cae265d0fd51e
Manish Giri <manish.giri.me@gmail.com>
2017-09-06 17:31:35 -0400

    Move double values with map to JS module

---
+++ JavaScript/use-map-to-double-the-values-in-an-array.js
@@ -0,0 +1,5 @@
+function double(array) {
+    // Use array.map() to return a new array with each value twice
+    // as large as the corresponding value in the passed in array.
+    return array.map(n => n * 2);
+}
