cfed569ed8834f1b331c6e055e4dd16884c60a6a
Manish Giri <manish.giri.me@gmail.com>
2017-09-07 03:02:40 -0400

    Solve JavaScript Array Filter

---
+++ JavaScript/javascript-array-filter.js
@@ -0,0 +1,12 @@
+/*
+JavaScript Arrays support a filter function (starting in JavaScript 1.6). Use the filter functionality to complete the function given.
+
+The solution would work like the following:
+
+getEvenNumbers([2,4,5,6]) // should == [2,4,6]
+*/
+
+function getEvenNumbers(numbersArray){
+  // filter out the odd numbers
+  return numbersArray.filter(e => e % 2 === 0);
+}
