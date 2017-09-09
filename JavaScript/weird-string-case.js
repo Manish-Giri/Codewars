9507e7b912458c57a70ea2549bd6cf90cd68df0e
Manish Giri <manish.giri.me@gmail.com>
2017-09-09 18:53:35 -0400

    Solve Weird String Case

---
+++ JavaScript/weird-string-case.js
@@ -0,0 +1,13 @@
+/*
+Write a function toWeirdCase (weirdcase in Ruby) that accepts a string, and returns the same string with all even indexed characters in each word upper cased, and all odd indexed characters in each word lower cased. The indexing just explained is zero based, so the zero-ith index is even, therefore that character should be upper cased.
+
+The passed in string will only consist of alphabetical characters and spaces(' '). Spaces will only be present if there are multiple words. Words will be separated by a single space(' ').
+
+Examples:
+
+toWeirdCase( "String" );//=> returns "StRiNg"
+toWeirdCase( "Weird string case" );//=> returns "WeIrD StRiNg Ca
+*/
+function toWeirdCase(string){
+  return string.trim().split(' ').map(word => word.split('').map((letter, i) => i % 2 === 0 ? letter.toUpperCase() : letter.toLowerCase()).join('')).join(' ');
+}
