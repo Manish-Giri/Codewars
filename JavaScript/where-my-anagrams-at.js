7968c0d32a95adf6f2ca59a8a1d49c1891f767a8
Manish Giri <manish.giri.me@gmail.com>
2017-09-11 01:57:29 -0400

    Solve Where my anagrams at?

---
+++ JavaScript/where-my-anagrams-at.js
@@ -0,0 +1,21 @@
+/*
+What is an anagram? Well, two words are anagrams of each other if they both contain the same letters. For example:
+
+'abba' & 'baab' == true
+
+'abba' & 'bbaa' == true
+
+'abba' & 'abbba' == false
+Write a function that will find all the anagrams of a word from a list. You will be given two inputs a word and an array with words. You should return an array of all the anagrams or an empty array if there are none. For example:
+
+anagrams('abba', ['aabb', 'abcd', 'bbaa', 'dada']) => ['aabb', 'bbaa']
+
+anagrams('racer', ['crazer', 'carer', 'racar', 'caers', 'racer']) => ['carer', 'racer']
+
+anagrams('laser', ['lazing', 'lazy',  'lacer']) => []
+*/
+
+function anagrams(word, words) {
+  return words.filter(e => e.length === word.length && check(word, e) && check(e, word));
+}
+const check = (foo, bar) => foo.split('').every(l => bar.includes(l));
