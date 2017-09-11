bee4f32092e235aa11aa0cc1dea04640490591da
Manish Giri <manish.giri.me@gmail.com>
2017-09-11 02:40:29 -0400

    Solve classy extensions

---
+++ JavaScript/classy-extentions.js
@@ -0,0 +1,13 @@
+/*
+Classy Extensions
+
+Classy Extensions, this kata is mainly aimed at the new JS ES6 Update introducing class extends You will be preloaded with the Animal class, so you should only edit the Cat class.
+Task
+
+Your task is to complete the Cat class which Extends Animal and replace the speak method to return the cats name + meows. e.g.
+'Mr Whiskers meows.'
+*/
+
+class Cat extends Animal {
+  speak() { return `${this.name} meows.` };
+}
