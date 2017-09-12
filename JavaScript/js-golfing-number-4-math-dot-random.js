09209e875be9873cde9229b220551b80dcc45817
Manish Giri <manish.giri.me@gmail.com>
2017-09-11 20:32:27 -0400

    Solve JS Golfing #4: Math.random

---
+++ JavaScript/js-golfing-number-4-math-dot-random.js
@@ -0,0 +1,10 @@
+/*
+Task:
+
+Return 'random' Numbers ( 0 < n < 100 ) with no more than 18 characters.
+
+The usage of global variables or arrays is not allowed
+If the test do not pass within the first time, try to submit again. (at least 50% of the numbers have to be unique) ;-)
+*/
+
+x=_=>Date.now();
