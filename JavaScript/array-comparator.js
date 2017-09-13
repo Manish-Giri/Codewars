4460074145e4e80d0a71458e8be4c7a968caedd3
Manish Giri <manish.giri.me@gmail.com>
2017-09-13 18:17:15 -0400

    Solve Array Comparator

---
+++ JavaScript/array-comparator.js
@@ -0,0 +1,5 @@
+// You have two arrays in this kata, every array contain only unique elements. Your task is to calcualate number of elements in first array which also are in second array.
+
+function matchArrays(v,r){
+ return v.filter(e => r.includes(e)).length;
+}
