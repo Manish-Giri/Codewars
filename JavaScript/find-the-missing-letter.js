94cdcac8883565e922f799c10a247d90b9fb6a5c
Manish Giri <manish.giri.me@gmail.com>
2017-09-29 18:55:45 -0400

    Solve Find the missing letter

---
+++ JavaScript/find-the-missing-letter.js
@@ -0,0 +1,24 @@
+/*
+#Find the missing letter
+
+Write a method that takes an array of consecutive (increasing) letters as input and that returns the missing letter in the array.
+
+You will always get an valid array. And it will be always exactly one letter be missing. The length of the array will always be at least 2.
+The array will always contain letters in only one case.
+
+Example:
+
+['a','b','c','d','f'] -> 'e'
+['O','Q','R','S'] -> 'P'
+
+(Use the English alphabet with 26 letters!)
+
+Have fun coding it and please don't forget to vote and rank this kata! :-)
+
+I have also created other katas. Take a look if you enjoyed this kata!
+*/
+
+function findMissingLetter(array) {
+  let n = array.map(e => e.charCodeAt(0));
+  return String.fromCharCode(n[n.findIndex((e,i,arr) => Math.abs(arr[i] - arr[i+1]) !== 1)]+1);
+}
