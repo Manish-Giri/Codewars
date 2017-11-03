be8a8b52ccce703cb4e5b3913ca72f2e674b3a8c
Manish Giri <manish.giri.me@gmail.com>
2017-11-03 01:28:49 -0400

    Solve Numbers in strings

---
+++ JavaScript/numbers-in-strings.js
@@ -0,0 +1,7 @@
+/*
+In this Kata, you will be given a string that has lowercase letters and numbers. Your task is to compare the number groupings and return the largest number.
+
+For example, solve("gh12cdy695m1") = 695, because this is the largest of all number groupings. 
+*/
+
+const solve = s => Math.max(...s.match(/\d+/g).map(e => +e));
