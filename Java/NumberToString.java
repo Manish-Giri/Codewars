f93a93ffab9accb13b8b71398cd98387d85ba61d
Manish Giri <manish.giri.me@gmail.com>
2017-11-05 00:55:57 -0400

    Solve Convert a Number to a String

---
+++ Java/NumberToString.java
@@ -0,0 +1,15 @@
+/*
+We need a function that can transform a number into a string.
+
+What ways of achieving this do you know?
+Examples:
+
+Kata.numberToString(123); // returns "123";   
+Kata.numberToString(999); // returns "999";
+*/
+
+class NumberToString {
+  public static String numberToString(int num) {
+    return String.valueOf(num);
+  }
+}
