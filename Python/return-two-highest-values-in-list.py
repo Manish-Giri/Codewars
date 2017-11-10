651c3317eb7389b3bd705e1f365f35fb96baa00b
Manish Giri <manish.giri.me@gmail.com>
2017-11-10 16:36:56 -0500

    Solve Return Two Highest Values in List

---
+++ Python/return-two-highest-values-in-list.py
@@ -0,0 +1,21 @@
+"""
+In this kata, your job is to return the two highest values in a list, this doesn't include duplicates.
+
+When given an empty list, you should also return an empty list, no strings will be passed into the list.
+
+The return should also be ordered from highest to lowest.
+
+If the argument passed isn't a list, you should return false.
+
+Examples:
+
+two_highest([4, 10, 10, 9]) should return [10, 9]
+
+two_highest([]) should return []
+
+two_highest("test") should return False
+"""
+
+def two_highest(arg1):
+    return False if type(arg1) is not list else sorted(list(set(arg1)), reverse=True)[:2]
+    
