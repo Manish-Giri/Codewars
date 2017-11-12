801325560ce5880ea492d6d5f6215b6ceb88b143
Manish Giri <manish.giri.me@gmail.com>
2017-11-11 19:05:55 -0500

    Solve Convert PascalCase string into snake_case

---
+++ JavaScript/convert-pascalcase-string-into-snake-case.js
@@ -0,0 +1,21 @@
+/*
+Complete the function/method so that it takes CamelCase string and returns the string in snake_case notation. Lowercase characters can be numbers. If method gets number, it should return string.
+
+Examples:
+
+//  returns test_controller
+toUnderscore('TestController');
+
+// returns movies_and_books
+toUnderscore('MoviesAndBooks');
+
+// returns app7_test
+toUnderscore('App7Test');
+
+// returns "1"
+toUnderscore(1);
+*/
+
+function toUnderscore(string) {
+  return typeof string === "number" ? ''+string : string.replace(/([a-z\d])([A-Z])/g, '$1_$2').toLowerCase(); 
+}
