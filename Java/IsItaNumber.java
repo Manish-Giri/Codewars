76db7cdd6dc1ba9faa53af6c06cb90125bf1b396
Manish Giri <manish.giri.me@gmail.com>
2017-12-04 21:16:41 -0500

    Solve Is it a number?

---
+++ Java/IsItaNumber.java
@@ -0,0 +1,32 @@
+/*
+Given a string s, write a method (function) that will return true if its a valid single integer or floating number or false if its not.
+
+Valid examples, should return true:
+
+isDigit("3")
+isDigit("  3  ")
+isDigit("-3.23")
+
+should return false:
+
+isDigit("3-4")
+isDigit("  3   5")
+isDigit("3 5")
+isDigit("zero")
+*/
+
+
+
+public class IsItaNumber {
+
+  public boolean isDigit(String s)
+  {
+      try {
+         double num = Double.parseDouble(s.trim());
+         return true;
+      }
+      catch (NumberFormatException e) {
+        return false;
+      }
+  }
+}
