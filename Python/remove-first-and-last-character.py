c44ab880ec22322426878bbcb6897c7a90e9a71d
Manish Giri <manish.giri.me@gmail.com>
2017-12-08 23:12:17 -0500

    Solve Remove First and Last Character

---
+++ Python/remove-first-and-last-character.py
@@ -0,0 +1,6 @@
+"""
+It's pretty straightforward. Your goal is to create a function that removes the first and last characters of a string. You're given one parameter, except in C, where, to keep the difficulty at the level of the kata, you are given two parameters, the first a buffer with length exactly the same as the second parameter, the original string. You don't have to worry with strings with less than two characters.
+"""
+
+def remove_char(s):
+    return s[1:len(s)-1]
