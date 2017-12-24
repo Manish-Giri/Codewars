949582b3fb97318eed59f2fc71c2ed375e54ce0e
Manish Giri <manish.giri.me@gmail.com>
2017-12-23 23:24:43 -0500

    Solve Check the exam

---
+++ JavaScript/check-the-exam.js
@@ -0,0 +1,19 @@
+/*
+The first input array contains the correct answers to an exam, like ["a", "a", "b", "d"]. The second one is "answers" array and contains student's answers.
+
+The two arrays are not empty and are the same length. Return the score for this array of answers, giving +4 for each correct answer, -1 for each incorrect answer, and +0 for each blank answer(empty string).
+
+If the score < 0, return 0.
+
+For example:
+
+checkExam(["a", "a", "b", "b"], ["a", "c", "b", "d"]) → 6
+checkExam(["a", "a", "c", "b"], ["a", "a", "b",  ""]) → 7
+checkExam(["a", "a", "b", "c"], ["a", "a", "b", "c"]) → 16
+checkExam(["b", "c", "b", "a"], ["",  "a", "a", "c"]) → 0
+*/
+
+function checkExam(array1, array2) {
+ const score = array1.map((e,i) => array2[i] === "" ? 0: e === array2[i] ? 4 : -1).reduce((a,b) => a+b,0);
+ return score > 0 ? score : 0;
+}
