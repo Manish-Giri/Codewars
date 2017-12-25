fe5eb8d111c2598b077c8d5cfa11dd90c82d69a0
Manish Giri <manish.giri.me@gmail.com>
2017-12-24 22:08:30 -0500

    Create returning-strings.py

---
+++ Python/returning-strings.py
@@ -0,0 +1,4 @@
+# Make a function that will return a greeting statement that uses an input; your program should return, "Hello, <name> how are you doing today?".
+
+def greet(name):
+    return "Hello, {} how are you doing today?".format(name)
