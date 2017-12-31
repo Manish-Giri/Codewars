f40f0f69881b98a13f38a601f4277aa3c76254b5
Manish Giri <manish.giri.me@gmail.com>
2017-12-30 22:48:52 -0500

    Create multiply-the-strings-in-the-array.js

---
+++ JavaScript/multiply-the-strings-in-the-array.js
@@ -0,0 +1,9 @@
+/*
+You received an array with two strings. Create a function that will return their product as a string. E.g.
+
+arrMultiply(['9','6']) should return '54'
+*/
+
+function arrMultiply(arr){
+   return ''+(+arr[0]*+arr[1]); 
+}
