691e4995142f8d3c9a2cf0aa81191d90a58018ca
Manish Giri <manish.giri.me@gmail.com>
2018-01-06 23:23:17 -0500

    Create merge-two-sorted-arrays-into-one.js

---
+++ JavaScript/merge-two-sorted-arrays-into-one.js
@@ -0,0 +1,10 @@
+/*You are given two sorted arrays that both only contain integers. Your task is to find a way to merge them into a single one, sorted in asc order. Complete the function mergeArrays(arr1, arr2), where arr1 and arr2 are the original sorted arrays.
+
+You don't need to worry about validation, since arr1 and arr2 must be arrays with 0 or more Integers. If both arr1 and arr2 are empty, then just return an empty array.
+
+Note: arr1 and arr2 may be sorted in different orders. Also arr1 and arr2 may have same integers. Remove duplicated in the returned result.*/
+
+function  mergeArrays(arr1, arr2) {
+  return [...new Set([...arr1, ...arr2])].sort((a,b)=>a-b);
+}
+
