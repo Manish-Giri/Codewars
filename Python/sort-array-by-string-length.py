34f552c4285fae69296b7207bd7d2c89833bc9ac
Manish Giri <manish.giri.me@gmail.com>
2018-01-11 22:55:37 -0500

    Create sort-array-by-string-length.py

---
+++ Python/sort-array-by-string-length.py
@@ -0,0 +1,17 @@
+"""
+Write a function that takes an array of strings as an argument and returns a sorted array containing the same strings, ordered from shortest to longest.
+
+For example, if this array were passed as an argument:
+
+["Telescopes", "Glasses", "Eyes", "Monocles"]
+
+Your function would return the following array:
+
+["Eyes", "Glasses", "Monocles", "Telescopes"]
+
+All of the strings in the array passed to your function will be different lengths, so you will not have to decide how to order multiple strings of the same length.
+"""
+
+def sort_by_length(arr):
+    return sorted(arr, key=lambda e: len(e))   
+    
