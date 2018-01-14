6e86289aac1aca390e55ae1b584d5dd653dcef6b
Manish Giri <manish.giri.me@gmail.com>
2018-01-13 23:37:55 -0500

    Create 5-without-numbers.py

---
+++ Python/5-without-numbers.py
@@ -0,0 +1,11 @@
+"""
+Just for laugh Write a function that always return 5
+
+Sounds easy right?? just bear in mind that you can't use any character among: 0123456789*+-/
+
+Good luck :)
+"""
+
+def unusual_five():
+    return len("hello");
+    
