5dc11c46a5d73eed41c8a9a01504fea16b9febbe
Manish Giri <manish.giri.me@gmail.com>
2018-01-15 23:51:30 -0500

    Create no-loops-2-you-only-need-one.js

---
+++ JavaScript/no-loops-2-you-only-need-one.js
@@ -0,0 +1,12 @@
+/*
+ No Loops Allowed
+
+You will be given an array (a) and a value (x). All you need to do is check whether the provided array contains the value, without using a loop.
+
+Array can contain numbers or strings. X can be either. Return true if the array contains the value, false if not. With strings you will need to account for case.
+
+Looking for more, loop-restrained fun? Check out the other kata in the series:
+
+*/
+
+const check = (a,x) => a.includes(x);
