ea22a24925df4a4b8a548608e33f6cfdffd16ed2
Manish Giri <manish.giri.me@gmail.com>
2018-01-23 23:41:40 -0500

    Create kata-example-twist.js

---
+++ JavaScript/kata-example-twist.js
@@ -0,0 +1,11 @@
+/*
+This is an easy twist to the example kata (provided by Codewars when learning how to create your own kata).
+
+Add the value "codewars" to the array websites/Websites 1,000 times.
+
+var websites = []
+*/
+
+// add the value "codewars" to the websites array 1,000 times
+var websites = []
+for(let i = 0; i < 1000; i++) { websites.push("codewars"); }
