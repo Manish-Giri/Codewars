f1531996bc865ead4009f827f28dc118c8c684c5
Manish Giri <manish.giri.me@gmail.com>
2018-01-30 23:52:09 -0500

    Create -remove-a-exclamation-mark-from-the-end.js

---
+++ JavaScript/-remove-a-exclamation-mark-from-the-end.js
@@ -0,0 +1,21 @@
+/*
+Description:
+
+Remove a exclamation mark from the end of string. For a beginner kata, you can assume that the input data is always a string, no need to verify it.
+Examples
+
+remove("Hi!") === "Hi"
+remove("Hi!!!") === "Hi!!"
+remove("!Hi") === "!Hi"
+remove("!Hi!") === "!Hi"
+remove("Hi! Hi!") === "Hi! Hi"
+remove("Hi") === "Hi"
+
+Note
+
+Please don't post issue about difficulty or duplicate.
+*/
+
+function remove(s){
+  return s[s.length-1] === "!" ? s.slice(0, s.length-1) : s;
+}
