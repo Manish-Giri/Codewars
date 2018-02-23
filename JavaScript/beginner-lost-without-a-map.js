a80306890964c9b86ecde02a2a3d7ada7ab18dfe
Manish Giri <manish.giri.me@gmail.com>
2018-02-22 23:06:04 -0500

    Create beginner-lost-without-a-map.js

---
+++ JavaScript/beginner-lost-without-a-map.js
@@ -0,0 +1,13 @@
+/*
+Given and array of integers (x), return the array with each value doubled.
+
+For example:
+
+[1, 2, 3] --> [2, 4, 6]
+
+For the beginner, try to use the map method - it comes in very handy quite a lot so is a good one to know.
+*/
+
+function maps(x){
+ return x.map(e => e*2);
+}
