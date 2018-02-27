013c3446af85bbaa4026c5472a8cfeff3f1c5e4a
Manish Giri <manish.giri.me@gmail.com>
2018-02-26 23:59:17 -0500

    Create fun-with-es6-classes-number-1.js

---
+++ JavaScript/fun-with-es6-classes-number-1.js
@@ -0,0 +1,16 @@
+/*
+Fun with ES6 Classes #1 - People, people, people
+Time for some OOP fun!
+
+Define a class Person with the following properties:
+
+A constructor that accepts 4 arguments: firstName/FirstName (defaults to "John" if not set), lastName/LastName (defaults to "Doe" if not set), age/Age (defaults to 0 if not set) and gender/Gender (defaults to "Male" if not set). These should be stored in this.firstName/this.FirstName, this.lastName/this.LastName, this.age/this.Age and this.gender/this.Gender respectively.
+A method sayFullName/SayFullName that accepts no arguments and returns the full name (e.g. "John Doe")
+A class/static method greetExtraTerrestrials/GreetExtraTerrestrials that accepts one parameter raceName and returns "Welcome to Planet Earth raceName". For example, if the race name is "Martians", it should say "Welcome to Planet Earth Martians"
+You may use any valid syntax you like; however, it is highly recommended that you complete this Kata using ES6 syntax and features.
+*/
+
+class Person {
+  constructor(firstName="John", lastName="Doe", age=0, gender="Male") {  
+    this.
+}
