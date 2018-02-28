3f59e5e0411e4668006b0b9e834aa989c5b71dcd
Manish Giri <manish.giri.me@gmail.com>
2018-02-27 23:43:13 -0500

    Create template-strings.js

---
+++ JavaScript/template-strings.js
@@ -0,0 +1,18 @@
+/*
+Template Strings
+Template Strings, this kata is mainly aimed at the new JS ES6 Update introducing Template Strings
+
+Task
+Your task is to return the correct string using the Template String Feature.
+
+Input
+Two Strings, no validation is needed.
+
+Output
+You must output a string containing the two strings with the word
+' are '
+*/
+
+var TempleStrings = function(obj, feature) {
+  return `${obj} are ${feature}`;
+}
