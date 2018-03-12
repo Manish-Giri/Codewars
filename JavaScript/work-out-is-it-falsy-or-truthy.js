a2638f8a9e2ea6da4c4a7da079a070fd2e4021b6
Manish Giri <manish.giri.me@gmail.com>
2018-03-12 01:05:28 -0400

    Create work-out-is-it-falsy-or-truthy.js

---
+++ JavaScript/work-out-is-it-falsy-or-truthy.js
@@ -0,0 +1,25 @@
+/*
+An array will be passed to the function falsyOrTruthy() containing either both falsy and truthy values or only one type of them; if the number of the array's values is even return truthy values otherwise return falsy ones. In the cases when corresponding values are not present at all simply return an empty array.
+
+Examples:
+
+var arr = [false,NaN,NaN,4,5,{}];
+
+falsyOrTruthy(arr); // should return [4,5,{}]
+
+var arr = [false,NaN,NaN,4,5,1,{}];
+
+falsyOrTruthy(arr); // should return [false,NaN,NaN]
+
+var arr = [false,NaN];
+
+falsyOrTruthy(arr) // should return [];
+
+var arr = [1,2,3];
+
+falsyOrTruthy(arr) // should return [];
+*/
+
+function falsyOrTruthy(arr) {
+  return arr.length%2?arr.filter(e=>!e):arr.filter(e=>e);
+}
