910400bbc6cd19e371c54db9efadd7d0e030971e
Manish Giri <manish.giri.me@gmail.com>
2018-04-03 22:59:45 -0400

    Create sort-numbers.js

---
+++ JavaScript/sort-numbers.js
@@ -0,0 +1,12 @@
+/*
+Finish the solution so that it sorts the passed in array of numbers. If the function passes in an empty array or null/nil value then it should return an empty array.
+
+For example:
+
+solution([1, 2, 10, 50, 5]); // should return [1,2,5,10,50]
+solution(null); // should return []
+*/
+
+function solution(nums){
+  return nums && nums.length ? nums.sort((a,b)=>a-b): [];
+}
