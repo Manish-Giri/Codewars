e1ef20ad03665ec9a0bcd08b1c3f8d6fd92b3a25
Manish Giri <manish.giri.me@gmail.com>
2018-04-06 23:37:56 -0400

    Create multiply-characters.py

---
+++ Python/multiply-characters.py
@@ -0,0 +1,13 @@
+"""
+Here we have a function that help us spam our hearty laughter. But is not working! I need you to find out why...
+
+Expected results:
+
+spam(1)  ==> "hue"
+spam(6)  ==> "huehuehuehuehuehue"
+spam(14) ==> "huehuehuehuehuehuehuehuehuehuehuehuehuehue"
+"""
+
+def spam(number):
+    return "hue"*number
+    
