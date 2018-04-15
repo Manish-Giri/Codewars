eb60fd580f667ce4bf70f0c183a134a3b8731665
Manish Giri <manish.giri.me@gmail.com>
2018-04-14 21:12:07 -0400

    Create convert-a-string-to-an-array.py

---
+++ Python/convert-a-string-to-an-array.py
@@ -0,0 +1,11 @@
+"""
+Write a function to split a string and convert it into an array of words. For example:
+
+"Robin Singh" ==> ["Robin", "Singh"]
+
+"I love arrays they are my favorite" ==> ["I", "love", "arrays", 
+"""
+
+def string_to_array(s):
+    return s.split(" ")
+    
