3f7298e0a7fe53b6dbc2b5f2d8fd549f9e4b9e01
Manish Giri <manish.giri.me@gmail.com>
2018-04-19 22:13:40 -0400

    Create strip-url-params.js

---
+++ JavaScript/strip-url-params.js
@@ -0,0 +1,23 @@
+/*
+Complete the method so that it does the following:
+Removes any duplicate query string parameters from the url
+Removes any query string parameters specified within the 2nd argument (optional array)
+Examples:
+stripUrlParams('www.codewars.com?a=1&b=2&a=2') // returns 'www.codewars.com?a=1&b=2'
+stripUrlParams('www.codewars.com?a=1&b=2&a=2', ['b']) // returns 'www.codewars.com?a=1'
+stripUrlParams('www.codewars.com', ['b']) // returns 'www.codewars.com
+*/
+
+function stripUrlParams(url, paramsToStrip=[]){
+  if(!url.includes("?")) {
+    return url;
+  }
+  const pos = url.indexOf("?")+1;
+  let queryString = url.slice(pos);
+  let queryParams = queryString.split("&");
+  let map = {}, revised='';
+  queryParams.forEach(e => !map.hasOwnProperty(e[0]) && !paramsToStrip.includes(e[0]) ? map[e[0]] = e[2]: 0 );
+  Object.keys(map).forEach(e => revised += `${e}=${map[e]}&`);
+  revised = revised.replace(/&$/g, '');
+  return url.slice(0, pos) + revised;
+}
