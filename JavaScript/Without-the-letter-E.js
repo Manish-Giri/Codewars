7b94867af78d01f6407e97fa4934986e23a1d3f5
manish-giri <manish.giri.me@gmail.com>
2018-04-22 05:33:13 -0400

    some change?

--- JavaScript/Without-the-letter-E.js
+++ JavaScript/Without-the-letter-E.js
@@ -1,27 +1,13 @@
-/**
- * Created by manishgiri on 6/25/17.
- */
-
 /*
- How much "e" does it contains (case-insensitive) - return number as String.
- If given String doesn't contain any "e", return: There is no "e".
- If given String is empty, return empty String.
- If given String is null, return null
- */
+Is it possible to write a book without the letter 'e' ?
+Task
 
-function find_E(str){
-    if(str == "") {
-        return "";
-    }
-    if(str == null) {
-        return null;
-    }
+Given String str, return:
+How much "e" does it contains (case-insensitive) - return number as String.
+If given String doesn't contain any "e", return: "There is no "e"."
+If given String is empty, return empty String.
+If given String is null/None/nil, return null/None/nil*/
 
-   let eCount =  str.split('').filter(letter => letter.toLowerCase() === "e").length;
-
-    console.log(eCount);
-    return eCount ? eCount.toString() : 'There is no "e".';
+function findE(str){
+  return !str ? "" : !(/e/gi).test(str) ? 'There is no "e".' : ''+str.match(/e/gi).length;
 }
-
-
-find_E("English")
