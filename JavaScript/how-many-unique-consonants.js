2c9987da064bf9970999b628b7d77ee88ab5b1ce
Manish Giri <manish.giri.me@gmail.com>
2018-04-27 02:46:21 -0400

    Create how-many-unique-consonants.js

---
+++ JavaScript/how-many-unique-consonants.js
@@ -0,0 +1,24 @@
+/*
+Complete the function that counts the number of unique consonants in a string (made up of printable ascii characters).
+
+Consonants are letters used in English other than "a", "e", "i", "o", "u". We will count "y" as a consonant.
+
+Remember, your function needs to return the number of unique consonants - disregarding duplicates. For example, if the string passed into the function reads "add", the function should return 1 rather than 2, since "d" is a duplicate.
+
+Similarly, the function should also disregard duplicate consonants of differing cases. For example, "Dad" passed into the function should return 1 as "d" and "D" are duplicates.
+
+Examples
+"add" ==> 1
+"Dad" ==> 1
+"aeiou" ==> 0
+"sillystring" ==> 7
+"abcdefghijklmnopqrstuvwxyz" ==> 21
+"Count my unique consonants!!" ==> 7
+
+*/
+
+
+function countConsonants(str) {
+  const vowels = ['a', 'e', 'i', 'o', 'u'];
+  return [...new Set(str.toLowerCase().replace(/[^a-z]/gi, ''))].filter(e => !vowels.includes(e)).length;
+}
