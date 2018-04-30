5dafbc412111bb98d49bb0a1aaca2b50587b0873
Manish Giri <manish.giri.me@gmail.com>
2018-04-29 22:50:00 -0400

    Create YesOrNo.java

---
+++ Java/YesOrNo.java
@@ -0,0 +1,9 @@
+// https://www.codewars.com/kata/convert-boolean-values-to-strings-yes-or-no/train/java
+
+class YesOrNo
+{
+  public static String boolToWord(boolean b)
+  {
+    return b ? "Yes": "No";
+  } 
+}
