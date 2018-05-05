8313adbb1779f2cfc41889c45e2f70ee5747ff8d
Manish Giri <manish.giri.me@gmail.com>
2018-05-04 23:50:50 -0400

    Update three-added-characters.py

--- Python/three-added-characters.py
+++ Python/three-added-characters.py
@@ -6,3 +6,10 @@ def added_char(s1, s2):
           return x
   return [x for x in s2 if s2.count(x) > s1.count(x)][0]
   
+
+  # updated soln
+  
+def added_char(s1, s2):  
+  a = set(s1)
+  b = set(s2)        
+  return [x for x in s2 if s2.count(x) > s1.count(x)][0] if a <= b else list(b-a)[0]       
