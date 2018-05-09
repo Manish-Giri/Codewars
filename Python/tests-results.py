11157e5f34a5ec047ee7877f1cba66032ac56813
Manish Giri <manish.giri.me@gmail.com>
2018-05-08 23:17:07 -0400

    Add clever solution for Test's results

--- Python/tests-results.py
+++ Python/tests-results.py
@@ -15,3 +15,11 @@ def test(r):
         res.append('They did well')
     return res    
     
+# clever solution
+
+from statistics import mean
+
+def test(r):
+    dct = {'l': 0, 'a': 0, 'h': 0}
+    for n in r: dct[ 'lah'[(n>6) + (n>8)] ] += 1
+    return [round(mean(r), 3), dct] + ['They did well'] * (sum(dct.values()) == dct['h'])
