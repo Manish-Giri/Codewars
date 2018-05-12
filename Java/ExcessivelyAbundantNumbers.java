1f1f0c2a2ff1b4698cb7eefe7add690e478a081b
Manish Giri <manish.giri.me@gmail.com>
2018-05-11 23:15:58 -0400

    Create ExcessivelyAbundantNumbers.java

---
+++ Java/ExcessivelyAbundantNumbers.java
@@ -0,0 +1,21 @@
+/*
+An abundant number or excessive number is a number for which the sum of its proper divisors is greater than the number itself.
+
+The integer 12 is the first abundant number. Its proper divisors are 1, 2, 3, 4 and 6 for a total of 16 (> 12).
+
+Derive function abundantNumber(num)/abundant_number(num) which returns true/True/.true. if num is abundant, false/False/.false. if not.
+*/
+
+import java.util.*;
+import java.util.stream.*;
+public class ExcessivelyAbundantNumbers {
+    public static boolean abundantNumber(int num) {
+        List<Integer> divisors = new ArrayList<>();
+        IntStream.range(1, num).forEach(n -> {
+          if(num % n == 0) {
+            divisors.add(n);
+          }
+        });
+        return num < divisors.stream().mapToInt(i->i).sum();
+    }  
+}
