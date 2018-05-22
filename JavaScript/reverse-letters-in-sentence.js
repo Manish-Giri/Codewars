19c28212059d8d364e1d1a051a6c02db6815a37a
Manish Giri <manish.giri.me@gmail.com>
2018-05-21 23:15:33 -0400

    Create reverse-letters-in-sentence.js

---
+++ JavaScript/reverse-letters-in-sentence.js
@@ -0,0 +1,17 @@
+/*
+Take a sentence (string) and reverse each word in the sentence. Do not reverse the order of the words, just the letters in each word.
+
+If there is punctuation, it should be interpreted as a regular character; no special rules.
+
+If there is spacing before/after the input string, leave them there.
+
+String will not be empty.
+
+Examples
+"Hi mom" => "iH mom"
+" A fun little challenge! " => " A nuf elttil !egnellahc "
+*/
+
+function reverser(sentence) {
+		return sentence.replace(/[\w!.]+/g, e => [...e].reverse().join(''));
+}
