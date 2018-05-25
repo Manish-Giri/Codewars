1db53dbd25a12d16b6bb3b75dad6c833eb8d2ada
Manish Giri <manish.giri.me@gmail.com>
2018-05-25 08:13:26 -0400

    Create highest-rank-number-in-an-array.py

---
+++ Python/highest-rank-number-in-an-array.py
@@ -0,0 +1,9 @@
+# https://www.codewars.com/kata/highest-rank-number-in-an-array/train/python
+
+from collections import defaultdict
+def highest_rank(arr):
+    d = defaultdict(int)
+    for k in arr:
+      d[k] += 1
+    return max(list(d.keys())) if(len(set(d.values())) == 1) else max(d, key=d.get)
+  
