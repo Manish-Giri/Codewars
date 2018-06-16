abd5390f9f776f875f63589a285c4d74d723f92e
Manish Giri <manish.giri.me@gmail.com>
2018-06-16 18:15:26 -0400

    Create beginner-series-number-3-sum-of-numbers.js

---
+++ JavaScript/beginner-series-number-3-sum-of-numbers.js
@@ -0,0 +1,18 @@
+/*
+Given two integers a and b, which can be positive or negative, find the sum of all the numbers between including them too and return it. If the two numbers are equal return a or b.
+
+Note: a and b are not ordered!
+Examples
+
+GetSum(1, 0) == 1   // 1 + 0 = 1
+GetSum(1, 2) == 3   // 1 + 2 = 3
+GetSum(0, 1) == 1   // 0 + 1 = 1
+GetSum(1, 1) == 1   // 1 Since both are same
+GetSum(-1, 0) == -1 // -1 + 0 = -1
+GetSum(-1, 2) == 2  // -1 + 0 + 1 + 2 = 2
+*/
+
+function GetSum( a,b ) {
+  const arr = [a,b].sort((a,b)=>a-b);
+  return new Array(arr[1]-arr[0]+1).fill(arr[0]).map((e,i)=>e+i).reduce((a,b)=>a+b,0); 
+}
