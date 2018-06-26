52ffb3149846b341aa2c926bc1ba26df4302dc25
Manish Giri <manish.giri.me@gmail.com>
2018-06-25 23:36:25 -0400

    Create Isograms.java

---
+++ Java/Isograms.java
@@ -0,0 +1,14 @@
+/*
+An isogram is a word that has no repeating letters, consecutive or non-consecutive. Implement a function that determines whether a string that contains only letters is an isogram. Assume the empty string is an isogram. Ignore letter case.
+
+isIsogram "Dermatoglyphics" == true
+isIsogram "moose" == false
+isIsogram "aba" == false
+*/
+
+import java.util.*;
+public class Isograms {
+    public static boolean  isIsogram(String str) {
+        return str.length() == 0 ? true : str.length() == new HashSet<>(Arrays.asList(str.toLowerCase().split(""))).size();
+    } 
+}
