1665769aad0c95798383d9b8c4e27c98db146e34
Manish Giri <manish.giri.me@gmail.com>
2018-07-02 22:24:28 -0400

    Create remove-duplicate-words.js

---
+++ JavaScript/remove-duplicate-words.js
@@ -0,0 +1,15 @@
+/*
+Your task is to remove all duplicate words from string, leaving only single words entries.
+
+Example:
+
+Input:
+
+'alpha beta beta gamma gamma gamma delta alpha beta beta gamma gamma gamma delta'
+
+Output:
+
+'alpha beta gamma delta'
+*/
+
+const removeDuplicateWords = s => [...new Set(s.split(' '))].join(' ');
