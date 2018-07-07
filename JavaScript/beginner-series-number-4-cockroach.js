a4438353f423a30c0fb17d710f981cc692a93e0d
Manish Giri <manish.giri.me@gmail.com>
2018-07-06 22:58:35 -0400

    Create beginner-series-number-4-cockroach.js

---
+++ JavaScript/beginner-series-number-4-cockroach.js
@@ -0,0 +1,12 @@
+/*
+The cockroach is one of the fastest insects. Write a function which takes its speed in km per hour and returns it in cm per second, rounded down to the integer (= floored).
+
+For example:
+
+cockroachSpeed(1.08) == 30
+Note! The input is a Real number (actual type is language dependent) and is >= 0. The result should be an Integer.
+*/
+
+function cockroachSpeed(s) {
+  return Math.floor(s * 27.7777778);
+}
