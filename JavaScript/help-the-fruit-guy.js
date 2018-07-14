5e18e577641bdfda23da9f1a7e9f1506a2001da2
Manish Giri <manish.giri.me@gmail.com>
2018-07-13 23:07:54 -0400

    Create help-the-fruit-guy.js

---
+++ JavaScript/help-the-fruit-guy.js
@@ -0,0 +1,12 @@
+/*
+Our fruit guy has a bag of fruit (represented as an array of strings) where some fruits are rotten. He wants to replace all the rotten pieces of fruit with fresh ones. For example, given ["apple","rottenBanana","apple"] the replaced array should be ["apple","banana","apple"]. Your task is to implement a method that accepts an array of strings containing fruits should returns an array of strings where all the rotten fruits are replaced by good ones.
+
+Notes
+If the array is null/nil/None or empty you should return empty array ([]).
+The rotten fruit name will be in this camelcase (rottenFruit).
+The returned array should be in lowercase.
+*/
+
+function removeRotten(bagOfFruits){
+  return !bagOfFruits || !bagOfFruits.length ? [] : bagOfFruits.map(e => e.includes("rotten") ? e[6].toLowerCase() + e.slice(7): e);
+}
