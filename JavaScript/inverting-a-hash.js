13d3bb3bb1544a36dd5266135d2985bf2bc854da
Manish Giri <manish.giri.me@gmail.com>
2018-07-28 20:49:04 -0400

    Create inverting-a-hash.js

---
+++ JavaScript/inverting-a-hash.js
@@ -0,0 +1,37 @@
+/*
+Summary
+Given a Hash made up of keys and values, invert the hash by swapping them.
+
+Examples
+Given:
+
+  { a: '1',
+    b: '2',
+    c: '3' }
+
+Return:
+
+  { 1: 'a',
+    2: 'b',
+    3: 'c' }
+
+
+
+Given:
+
+  { foo:   'bar',
+    hello: 'world' }
+
+Return:
+
+  { bar:   'foo',
+    world: 'hello' }
+Notes
+Keys and values may be of any type appropriate for use as a key.
+All hashes provided as input will have unique values, so the inversion is involutive. In other words, do not worry about identical values stored under different keys.
+*/
+
+const _ = require('lodash');
+function invertHash(hash) {
+  return _.invert(hash);
+}
